693cdcdac13eb21753cb3bdfd188f60480585d16
Dinah Deshane <dinahdeshane@gmail.com>
2018-12-13 19:46:23 -0500

    customers

--- server.js
+++ server.js
@@ -20,3 +20,41 @@ var server = http.createServer(handleRequest);
 server.listen(PORT,function(){
     console.log("Server listening on: http://localhost:" + PORT);
 });
+
+var customers = [
+    {
+        routeName:
+        name:
+        phoneNumber:
+        email:
+        uniqueID:
+    },
+    {
+        routeName:
+        name:
+        phoneNumber:
+        email:
+        uniqueID:
+    },
+    {
+        routeName:
+        name:
+        phoneNumber:
+        email:
+        uniqueID:
+    },
+    {
+        routeName:
+        name:
+        phoneNumber:
+        email:
+        uniqueID:
+    },
+    {
+        routeName:
+        name:
+        phoneNumber:
+        email:
+        uniqueID:
+    },
+]
